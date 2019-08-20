1078045a892ad0c91646e83c1c1a986f054f8779
Eric Fuller <efuller82@gmail.com>
2019-08-20 11:58:06 -0500

    basic home.html setup

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+var friends = [
+    {
+        name: 'Roseanne',
+        photo: 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiZtbm865HkAhVEgK0KHQZSCQoQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.thewrap.com%2Froseanne-fired-again-self-destruction%2F&psig=AOvVaw3B1PhCl4RneOY91Ye-XW14&ust=1566403915160422',
+        scores: [5, 3, 2, 3, 3, 4, 3, 3, 2, 3,]
+    }
+]
+
+module.exports = friends;
