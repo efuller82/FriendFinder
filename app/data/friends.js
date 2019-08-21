fa1e7e55fe292256fe163b178deb8567d38126ee
Eric Fuller <efuller82@gmail.com>
2019-08-21 12:11:02 -0500

    all friends and photos added

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,53 @@
 var friendsArr = [
     {
         name: 'Roseanne',
-        photo: 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiZtbm865HkAhVEgK0KHQZSCQoQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.thewrap.com%2Froseanne-fired-again-self-destruction%2F&psig=AOvVaw3B1PhCl4RneOY91Ye-XW14&ust=1566403915160422',
-        scores: [5, 3, 2, 3, 3, 4, 3, 3, 2, 3,]
+        photo: 'https://www.thewrap.com/wp-content/uploads/2018/05/rose-1.jpg',
+        scores: [5, 3, 2, 3, 3, 4, 3, 3, 2, 3]
+    },
+    {
+        name: 'Steve Buscemi',
+        photo: 'https://i.ytimg.com/vi/VDLMTx6S8Wk/maxresdefault.jpg',
+        scores: [2, 3, 5, 3, 2, 4, 1, 2, 4, 3]
+    },
+    {
+        name: 'Mama June',
+        photo: 'https://www.thesun.co.uk/wp-content/uploads/2019/08/NINTCHDBPICT000304222356.jpg',
+        scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    },
+    {
+        name: 'Louie Anderson',
+        photo: 'http://hollywoodmask.com/uploads/images/louie-anderson.jpg',
+        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    {
+        name: "Rosie O//'Donnell",
+        photo: 'https://mondrian.mashable.com/uploads%252Fcard%252Fimage%252F378842%252Fd831bed8-8271-485b-bc56-0f70fc2bc478.png%252Ffull-fit-in__950x534.png?signature=x0v6_il6JyoDR3S1NIX8nMs4o8g=&source=https%3A%2F%2Fblueprint-api-production.s3.amazonaws.com',
+        scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    },
+    {
+        name: 'Mr. Bean',
+        photo: 'https://img.izismile.com/img/img4/20110919/640/famous_actors_who_got_hit_with_the_ugly_stick_640_19.jpg',
+        scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+    {
+        name: 'Drunk Nick Nolte',
+        photo: 'http://listverse-wpengine.netdna-ssl.com/wp-content/uploads/2007/09/nolte.png',
+        scores: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+    },
+    {
+        name: 'The Penguin',
+        photo: 'http://www.mwctoys.com/images2/review_cobblepot_1.jpg',
+        scores: [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
+    },
+    {
+        name: 'Janet Reno',
+        photo: 'http://listverse.com/wp-content/uploads/2007/09/msreno.jpg',
+        scores: [5, 5, 4, 4, 3, 3, 2, 2, 1, 1]
+    },
+    {
+        name: 'New York from Flava of Love',
+        photo: 'https://i.guim.co.uk/img/static/sys-images/Arts/Arts_/Pictures/2008/11/21/fla460.jpg?width=620&quality=45&auto=format&fit=max&dpr=2&s=cfcdc1bf7a6dedf3c2cb0ee87ecbb135',
+        scores: [4, 3, 4, 5, 1, 3, 4, 2, 5, 2]
     }
 ]
 
