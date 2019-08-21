44b10705a60f6fac20cc7dde2a962b06f94f2c47
Eric Fuller <efuller82@gmail.com>
2019-08-21 11:45:08 -0500

    form data added to friends api

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,8 +5,12 @@ module.exports = function (app) {
     // GET route that will display JSON of all possible friends
     app.get('/api/friends', function (req, res) {
         res.json(friendsData);
-    })
+    });
 
 
     // POST Route handle incoming survey results and also will be used to handle compatibility logic
+    app.post('/api/friends', function (req, res) {
+        friendsData.push(req.body);
+        res.json(true);
+    })
 }
