13f38efa01568112be5c093a7f949aa7f347e570
Eric Fuller <efuller82@gmail.com>
2019-08-21 07:24:15 -0500

    server up and running

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
-var express = require('express');
 var bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 8080;
 var path = require('path');
 
-var app = express();
-var port = process.env.PORT || 3000;
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
+
 
