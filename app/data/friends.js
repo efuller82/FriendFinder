eb6d002d1fed4d2e6f40e826806e897c338e4f7d
Eric Fuller <efuller82@gmail.com>
2019-08-21 08:54:54 -0500

    form now pushes values to array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,4 @@
-var friends = [
+var friendsArr = [
     {
         name: 'Roseanne',
         photo: 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiZtbm865HkAhVEgK0KHQZSCQoQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.thewrap.com%2Froseanne-fired-again-self-destruction%2F&psig=AOvVaw3B1PhCl4RneOY91Ye-XW14&ust=1566403915160422',
@@ -6,4 +6,4 @@ var friends = [
     }
 ]
 
-module.exports = friends;
+module.exports = friendsArr;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,12 @@
-
+var friendsData = require('../data/friends.js');
 
 
 module.exports = function (app) {
+    // GET route that will display JSON of all possible friends
+    app.get('/api/friends', function (req, res) {
+        res.json(friendsData);
+    })
 
+
+    // POST Route handle incoming survey results and also will be used to handle compatibility logic
 }
